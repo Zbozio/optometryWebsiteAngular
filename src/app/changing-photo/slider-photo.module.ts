9cf491ce9ddefacce76865cdb1d7ad51b1bdc77e
Bozio <karollos.bozek@gmail.com>
2024-04-13 17:49:54 +0200

    slider

--- src/app/changing-photo/slider-photo.module.ts
+++ src/app/changing-photo/slider-photo.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChangingPhotoComponent } from './changing-photo.component';
-import { NgImageSliderModule } from 'ng-image-slider'; 
+import { NgImageSliderModule } from 'ng-image-slider';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 
 @NgModule({
   declarations: [
     ChangingPhotoComponent
   ],
   imports: [
-    CommonModule, NgImageSliderModule
+    CommonModule,NgImageSliderModule
   ],
   exports: [
     ChangingPhotoComponent
