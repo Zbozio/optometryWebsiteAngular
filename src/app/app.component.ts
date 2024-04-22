b06dbfb138133100d0340250edc85f39115a36ab
Bozio <karollos.bozek@gmail.com>
2024-04-22 18:30:21 +0200

    working on outly site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'relationshipPage';
-  loadedFeature='';
+  loadedFeature='Home';
 
   onNavigate(feature:string){
     this.loadedFeature=feature;
